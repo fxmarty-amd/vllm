6ba99037bbda0596ba0a347ff252ba0b87d625d5
Felix Marty <Felix.Marty@amd.com>
2026-02-10 09:52:53 -0600

    squash

--- vllm/benchmarks/sweep/plot.py
+++ vllm/benchmarks/sweep/plot.py
@@ -27,6 +27,63 @@ except ImportError:
     seaborn = PlaceholderModule("seaborn")
 
 
+
+import matplotlib.scale as mscale
+import matplotlib.pyplot as plt
+import matplotlib.transforms as mtransforms
+import matplotlib.ticker as ticker
+import numpy as np
+
+class SquareRootScale(mscale.ScaleBase):
+    """
+    ScaleBase class for generating square root scale.
+    """
+ 
+    name = 'squareroot'
+ 
+    def __init__(self, axis, **kwargs):
+        # note in older versions of matplotlib (<3.1), this worked fine.
+        # mscale.ScaleBase.__init__(self)
+
+        # In newer versions (>=3.1), you also need to pass in `axis` as an arg
+        mscale.ScaleBase.__init__(self, axis)
+ 
+    def set_default_locators_and_formatters(self, axis):
+        axis.set_major_locator(ticker.AutoLocator())
+        axis.set_major_formatter(ticker.ScalarFormatter())
+        axis.set_minor_locator(ticker.NullLocator())
+        axis.set_minor_formatter(ticker.NullFormatter())
+ 
+    def limit_range_for_scale(self, vmin, vmax, minpos):
+        return  max(0., vmin), vmax
+ 
+    class SquareRootTransform(mtransforms.Transform):
+        input_dims = 1
+        output_dims = 1
+        is_separable = True
+ 
+        def transform_non_affine(self, a): 
+            return np.array(a)**0.5
+ 
+        def inverted(self):
+            return SquareRootScale.InvertedSquareRootTransform()
+ 
+    class InvertedSquareRootTransform(mtransforms.Transform):
+        input_dims = 1
+        output_dims = 1
+        is_separable = True
+ 
+        def transform(self, a):
+            return np.array(a)**2
+ 
+        def inverted(self):
+            return SquareRootScale.SquareRootTransform()
+ 
+    def get_transform(self):
+        return self.SquareRootTransform()
+    
+mscale.register_scale(SquareRootScale)
+
 @dataclass
 class PlotFilterBase(ABC):
     var: str
@@ -367,10 +424,24 @@ def _plot_fig(
             style=style,
             size=size,
             markers=True,
+            marker="o",
+            linestyle="--",
             errorbar="sd" if error_bars else None,
         )
 
-        g.add_legend(title=hue)
+        # g.add_legend(title=hue)
+
+        # g.set(reset)
+
+        for ax in g.axes.flatten():
+            ax.grid(True)
+
+        tick_positions = sorted(df[var_x].unique())
+        tick_labels = tick_positions
+
+        g.set(xticks=tick_positions, xticklabels=tick_labels)
+
+        g.add_legend(title=hue, loc="lower left", bbox_to_anchor=(0, -0.15))
     else:
         df["curve_group"] = (
             pd.concat(
@@ -392,6 +463,7 @@ def _plot_fig(
 
         g.add_legend()
 
+    g.set(ylim=(0, None))
     g.savefig(fig_path, dpi=fig_dpi)
     plt.close(g.figure)
 
