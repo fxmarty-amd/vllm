12671ceba06f0e18dbc625b048289e3dc2505ad5
Felix Marty <Felix.Marty@amd.com>
2026-09-01 02:14:33 -0500

    get_effective_quant_method -> resolve_quant_method renaming, avoid unnecessary SimpleNamespace/Mock in tests
    Signed-off-by: Felix Marty <Felix.Marty@amd.com>

--- vllm/model_executor/layers/vocab_parallel_embedding.py
+++ vllm/model_executor/layers/vocab_parallel_embedding.py
@@ -23,6 +23,7 @@ from vllm.model_executor.layers.quantization.base_config import (
     QuantizationConfig,
     QuantizeMethodBase,
     method_has_implemented_embedding,
+    resolve_quant_method,
 )
 from vllm.model_executor.layers.utils import dispatch_unquantized_gemm
 from vllm.model_executor.parameter import BasevLLMParameter
@@ -283,7 +284,7 @@ class VocabParallelEmbedding(PluggableLayer):
 
         quant_method = None
         if quant_config is not None:
-            quant_method = quant_config.get_effective_quant_method(self, prefix=prefix)
+            quant_method = resolve_quant_method(quant_config, self, prefix=prefix)
         if quant_method is None:
             quant_method = UnquantizedEmbeddingMethod()
 
